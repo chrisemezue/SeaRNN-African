a8d353d1f4658bcc2e60c387ba7ae71f7d2c0fff
chrisemezue <chris.emezue@gmail.com>
2024-04-29 10:40:36 -0400

    slurm files

--- run.sh
+++ run.sh
@@ -7,3 +7,12 @@ do
     sbatch job.sh fr ewe $type
     sbatch job.sh en ibo $type
 done
+
+
+
+# Submitted batch job 4672546
+# Submitted batch job 4672547
+# Submitted batch job 4672548
+# Submitted batch job 4672549
+# Submitted batch job 4672550
+# Submitted batch job 4672551
